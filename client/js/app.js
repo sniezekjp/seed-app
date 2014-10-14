5ebf56f897e97b27a479e904ebfb8811f456d04b
JP <jsniezek@crowdskout.com>
2014-10-14 08:58:58 -0400

    Updated to angular 1.3

--- client/js/app.js
+++ client/js/app.js
@@ -1,45 +1,5 @@
-var app = angular.module('app', ['ngResource']);
+var app = angular.module('app', []);
 
-app.constant('ENV', {
-  apiUrl: 'http://localhost:3000'
-});
-
-app.constant('Models', {
-  list: ['Account']
-});
-
-app.factory('AccountModel', function($resource) {
-  return function AccountModel(props) {
-    var resource = $resource('/account', {}, {});
-    return new resource(props);
-  }
-});
-
-app.factory('ModelBuilder', function(Models, $injector) {
-  var models = {};
-  Models.list.forEach(function(model) {
-    models[model] = $injector.get(model + 'Model');
-  });
-  return {
-    getModel: function(name) {
-      return models[name];
-    }
-  }
-});
-
-
-app.config(function($provide) {
-  console.log('config');
-});
-
-app.controller('MainCtrl', function(ModelBuilder) {
+app.controller('MainCtrl', function() {
   console.log('controller');
-  var Account = ModelBuilder.getModel('Account');
-  var account = new Account({id: 1});
-  account.$save().then(function(account) {
-    account.name = "New Name"; 
-    account.$save().then(function(account) {
-      console.log(account);
-    });
-  });
 });
